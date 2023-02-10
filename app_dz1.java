1f3c3cc92cec38163b8f5474a72dc9c74245e3ef
GromDv <gromdv@gmail.com>
2023-02-10 13:42:53 +0300

    Add main method

--- app_dz1.java
+++ app_dz1.java
@@ -4,6 +4,12 @@
 public class app_dz1 {
 
     public static void main(String[] args) {
-        
+
+        System.out.println(GetTriangleNum(3));
+
+    }
+
+    public static int GetTriangleNum(int n) {
+        return n * (n + 1) / 2;
     }
 }
