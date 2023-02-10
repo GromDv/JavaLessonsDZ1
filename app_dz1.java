c0e5072f3531b3aa1e7c05087f8319167245f5cd
GromDv <gromdv@gmail.com>
2023-02-10 13:55:52 +0300

    Add user input Scanner

--- app_dz1.java
+++ app_dz1.java
@@ -1,15 +1,25 @@
+import java.util.Scanner;
+
 /**
  * app_dz1
  */
 public class app_dz1 {
 
+    private static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        System.out.println(GetTriangleNum(3));
+        int num = getNumberByUser("Введите номер искомого треугольного числа: ");
+        System.out.printf("Треугольное число номер %d равно %d.\n", num, GetTriangleNum(num));
 
     }
-
+    
     public static int GetTriangleNum(int n) {
         return n * (n + 1) / 2;
+    }
+
+    public static int getNumberByUser(String text) {
+        System.out.print(text);
+        return input.nextInt();
     }
 }
